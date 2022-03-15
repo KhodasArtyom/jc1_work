98286e4350aae237ce504d8daa0e0ae669da2e73
ArtyomKhodas <gob1in@Mail.ru>
2022-03-15 21:10:48 +0300

    Regular exceptions

---
+++ src/my/pattern/RegExpStr.java
@@ -0,0 +1,8 @@
+package my.pattern;
+
+public class RegExpStr {
+    public static void main(String[] args) {
+        String s1 =  "12";
+        System.out.println(s1.matches("-?\\d\\d"));
+    }
+}
