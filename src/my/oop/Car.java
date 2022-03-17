7f43b4d3fe45439c67a676f50966e3a41064032d
ArtyomKhodas <gob1in@Mail.ru>
2022-03-17 21:25:14 +0300

    OOP

---
+++ src/my/oop/Car.java
@@ -0,0 +1,20 @@
+package my.oop;
+
+public class Car extends Machine implements Movable,Breakable{
+
+    @Override
+    public void move() {
+        System.out.println("Car is moving...");
+
+    }
+
+    @Override
+    public void doBreak() {
+        System.out.println("Car stopped");
+    }
+
+    @Override
+    public void work() {
+
+    }
+}
---
+++ src/my/oop/Machine.java
@@ -0,0 +1,11 @@
+package my.oop;
+
+public abstract class Machine {
+
+    public Machine() {
+
+    }
+
+    public abstract void work();
+
+}
